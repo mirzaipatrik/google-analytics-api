d173d9e3b93bad314584c09e11b21e8708e65087
Patrik Mirzai <mirzaipatrik@OMT-Mac-10.local>
2023-06-18 16:24:42 +0300

    feat: google-analytics-historical-data

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const { google } = require("googleapis");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const fs = require('fs').promises;
+
+dotenv.config();
 
 const scopes = "https://www.googleapis.com/auth/analytics.readonly";
 const view_id = process.env.VIEW_ID;
@@ -13,32 +16,67 @@ const jwt = new google.auth.JWT(
         scopes
 );
 
+const startDate = new Date("2023-01-01");
+const endDate = new Date("2023-06-01");
+const dateRange = getDateRange(startDate, endDate, 30); // Assuming 30-day intervals
+
 async function getViews() {
         try {
-                await jwt.authorize();
+                const obj = { data: [] };
 
-                const response = await google.analytics("v3").data.ga.get({
-                        auth: jwt,
-                        ids: "ga:" + view_id,
-                        "start-date": "2021-01-01",
-                        "end-date": "today",
-                        metrics: "ga:pageviews",
-                        dimensions: "ga:month,ga:pagePath",
-                        sort: "-ga:pageviews",
-                        "max-results": 100, // Adjust the number of top pages as needed
-                });
+                for (const range of dateRange) {
+                        const formattedStartDate = formatDate(range.start);
+                        const formattedEndDate = formatDate(range.end);
 
-                response.data.rows.map(row => {
-                        console.log(row)
-                });
+                        const response = await google.analytics("v3").data.ga.get({
+                                auth: jwt,
+                                ids: "ga:" + view_id,
+                                "start-date": formattedStartDate,
+                                "end-date": formattedEndDate,
+                                metrics: "ga:pageviews",
+                                dimensions: "ga:month, ga:year, ga:pagePath",
+                                sort: "ga:year, ga:month, -ga:pageviews",
+                                "max-results": 10000, // Adjust as needed (maximum value: 10000)
+                        });
+                        response.data.rows.map(row => obj.data.push(({
+                                year: row[1],
+                                month: row[0],
+                                page: row[2],
+                                views: row[3]
+                        })))
+                }
 
+                const json = JSON.stringify(obj);
+                await fs.writeFile('historica-bahai-org-pageviews.json', json, 'utf8');
+
+                console.log("JSON file written successfully.");
         } catch (err) {
                 console.log(err);
         }
-};
+}
 
-getViews();
+function getDateRange(startDate, endDate, intervalDays) {
+        const dateRange = [];
+        let currentDate = new Date(startDate);
+
+        while (currentDate <= endDate) {
+                const nextDate = new Date(currentDate);
+                nextDate.setDate(currentDate.getDate() + intervalDays);
+                dateRange.push({ start: new Date(currentDate), end: new Date(nextDate) });
+                currentDate = nextDate;
+        }
+
+        return dateRange;
+}
+
+function formatDate(date) {
+        return date.toISOString().slice(0, 10);
+}
+
+(async () => {
+        await getViews();
+})();
 
 app.listen(3000, () => {
-        console.log("Listening of port 3000")
-})
+        console.log("Listening on port 3000");
+});
